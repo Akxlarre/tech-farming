466134919ecff8dfed7d7b9aceb8f5bd1f5b0d08
Akxlarre <b.rebolledo@duocuc.cl>
2025-03-30 21:06:58 -0300

    fix: Agregar columna 'parametros' al modelo TipoSensor

--- tech-farming-backend/app/models/tipo_sensor.py
+++ tech-farming-backend/app/models/tipo_sensor.py
@@ -4,6 +4,7 @@ class TipoSensor(db.Model):
     __tablename__ = 'tipos_sensor'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(50))
+    parametros = db.Column(db.String(50))
     unidad = db.Column(db.String(20))
 
     parametros = db.relationship('SensorParametro', backref='tipo_sensor', lazy=True)
