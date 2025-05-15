007e88e384166bcc6426a70adc8b268b52de3735
SorkoTheProgram <ig.sorko@duocuc.cl>
2025-05-15 04:12:50 -0400

    Se Crea Zona Queries y Zona.py. Se crea api para listar zonas de un invernadero

--- tech-farming-backend/app/routes/__init__.py
+++ tech-farming-backend/app/routes/__init__.py
@@ -3,6 +3,7 @@ from app.routes.sensores import router as sensores_bp
 from app.routes.invernaderos import router as invernaderos_bp
 from app.routes.tipos_sensor import router as tipos_sensor_bp
 from app.routes.tipos_parametro import router as tipos_parametro_bp
+from app.routes.zona import router as zonas_bp
 
 def register_routes(app):
     app.register_blueprint(api_bp, url_prefix='/api')
@@ -10,3 +11,4 @@ def register_routes(app):
     app.register_blueprint(invernaderos_bp, url_prefix='/api/invernaderos')
     app.register_blueprint(tipos_sensor_bp, url_prefix='/api/tipos-sensor')
     app.register_blueprint(tipos_parametro_bp, url_prefix='/api/tipos-parametro')
+    app.register_blueprint(zonas_bp, url_prefix='/api/invernaderos')
