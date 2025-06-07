6b63f7d5705ec17bd3e24716b50f4295af6fc90d
Akxlarre <b.rebolledo@duocuc.cl>
2025-06-07 00:13:34 -0400

    fix: agregar CommonModule a las importaciones del componente Splash

--- tech-farming-frontend/src/app/core/components/splash/splash.component.ts
+++ tech-farming-frontend/src/app/core/components/splash/splash.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { AppLogoComponent } from "../app-logo.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-splash',
   standalone: true,
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.css'],
-  imports: [AppLogoComponent],
+  imports: [CommonModule, AppLogoComponent],
 })
 export class SplashComponent {
   @Input() fadeOut = false;
