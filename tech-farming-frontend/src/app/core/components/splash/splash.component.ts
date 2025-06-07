7aaabc6cd89e2808eb2501c73fd406aa8d624149
m-fuentesr <163899771+m-fuentesr@users.noreply.github.com>
2025-06-06 21:17:42 -0400

    Actualizado requirements.txt para que no de errores y arreglado pequeño error en splash

--- tech-farming-frontend/src/app/core/components/splash/splash.component.ts
+++ tech-farming-frontend/src/app/core/components/splash/splash.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AppLogoComponent } from '../app-logo.component';
 
 @Component({
   selector: 'app-splash',
   standalone: true,
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.css'],
+  imports: [AppLogoComponent],
 })
 export class SplashComponent {}
