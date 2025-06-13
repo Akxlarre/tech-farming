3b7c1fa09d3e2339a379b1c9e92f9c795a7c6093
Benjamin Andres Rebolledo Rebolledo <105505096+Akxlarre@users.noreply.github.com>
2025-06-13 03:24:38 -0400

    refactor: load Supabase key from env

--- tech-farming-frontend/src/environments/environment.ts
+++ tech-farming-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://efejsncxndycbgfyhvcv.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmZWpzbmN4bmR5Y2JnZnlodmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwMzE0NjYsImV4cCI6MjA1ODYwNzQ2Nn0.CxHCTJkNFrT4BOZCnFTB3pylfxc-2w-mPtS5UXs7nHs'
 };
